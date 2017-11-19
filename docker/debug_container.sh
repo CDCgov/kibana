1210428747315cfe3df103ad297ff0b5c4881212
hkong <hkong@mitre.org>
2017-11-19 16:52:08 -0500

    clean up

--- docker/debug_container.sh
+++ docker/debug_container.sh
@@ -12,5 +12,5 @@ curl -XGET ${ELASTICSEARCH_URL}
 echo "show configs..."
 cat ${KIBANA_CONFIG}/kibana.yml
 
-echo "adding a single document to ${ELASTICSEARCH_URL}/test/ index"
-curl -XPUT ${ELASTICSEARCH_URL}/test/external/1 -H 'Content-Type: application/json' -d'{"name": "ocp-kibana container test"}'
+#echo "adding a single document to ${ELASTICSEARCH_URL}/test/ index"
+#curl -XPUT ${ELASTICSEARCH_URL}/test/external/1 -H 'Content-Type: application/json' -d'{"name": "ocp-kibana container test"}'
